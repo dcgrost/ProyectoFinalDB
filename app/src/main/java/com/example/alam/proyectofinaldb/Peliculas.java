b4699e421f001c9ce3da4bd1e88a873efeb6b8bb
DCGrost <alamjgl_2@hotmail.com>
2018-06-10 22:15:22 -0500

    Antes de poner código de Oscar.

--- app/src/main/java/com/example/alam/proyectofinaldb/Peliculas.java
+++ app/src/main/java/com/example/alam/proyectofinaldb/Peliculas.java
@@ -3,6 +3,8 @@ package com.example.alam.proyectofinaldb;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.alam.proyectofinaldb.Utilities.Data_utilities;
+
 public class Peliculas extends AppCompatActivity {
 
     @Override
@@ -13,6 +15,6 @@ public class Peliculas extends AppCompatActivity {
         String tipo = getIntent().getStringExtra("tipo");
         String genero = getIntent().getStringExtra("genero");
 
-
+        String query = "select "+Data_utilities.PcampoImg+", "+Data_utilities.PcampoTitulo+", "+Data_utilities.PcampoSinopsis+" from "+tipo+" where "+Data_utilities.PcampoGenero+" ='"+genero+"'";
     }
 }
