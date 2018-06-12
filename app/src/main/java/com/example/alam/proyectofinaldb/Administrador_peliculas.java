74a692c361a6b84663fa4092111576a05baeaac6
DCGrost <alamjgl_2@hotmail.com>
2018-06-11 22:40:28 -0500

    Sigo sin poder poner las imagenes ni tampoco se ven

--- app/src/main/java/com/example/alam/proyectofinaldb/Administrador_peliculas.java
+++ app/src/main/java/com/example/alam/proyectofinaldb/Administrador_peliculas.java
@@ -1,5 +1,6 @@
 package com.example.alam.proyectofinaldb;
 
+import android.annotation.SuppressLint;
 import android.content.ContentValues;
 import android.content.Intent;
 import android.database.Cursor;
