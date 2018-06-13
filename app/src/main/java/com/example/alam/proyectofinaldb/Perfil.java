34ad513585cbde1e9fc48967c464ebb416ae28cc
DCGrost <alamjgl_2@hotmail.com>
2018-06-12 22:10:55 -0500

    Activity perfil creado falta boton de entrada

---
+++ app/src/main/java/com/example/alam/proyectofinaldb/Perfil.java
@@ -0,0 +1,42 @@
+package com.example.alam.proyectofinaldb;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.alam.proyectofinaldb.Utilities.Data_utilities;
+
+public class Perfil extends AppCompatActivity {
+
+    TextView tv_nombre, tv_correo, tv_fechaN, tv_saldo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perfil);
+
+        tv_nombre = (TextView)findViewById(R.id.nombre);
+        tv_correo = (TextView)findViewById(R.id.correo);
+        tv_fechaN = (TextView)findViewById(R.id.fechaN);
+        tv_saldo = (TextView)findViewById(R.id.saldo);
+
+        String userId = getIntent().getStringExtra("userId");
+
+        AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this, "administrar", null, 1);
+        SQLiteDatabase db = admin.getWritableDatabase();
+
+        String nombre = userId;
+        String[] parametro = {nombre};
+        String[] campos = {Data_utilities.UcampoNombre, Data_utilities.UcampoCorreo, Data_utilities.UcampoFechaN, Data_utilities.UcampoSaldo};
+
+        Cursor fila = db.query(Data_utilities.tablaUsuarios, campos, Data_utilities.UcampoNombre+ "=?", parametro, null, null, null);
+        fila.moveToFirst();
+        tv_nombre.setText(fila.getString(0));
+        tv_correo.setText(fila.getString(1));
+        tv_fechaN.setText(fila.getString(2));
+        tv_saldo.setText(fila.getString(3));
+        fila.close();
+    }
+}
